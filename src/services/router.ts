dc7a5c10d92658b8d94d593d2e31b0b334938393
Baranin67 <mati.baranowicz@gmail.com>
2025-05-01 20:34:27 +0200

    update: add router service

---
+++ src/services/router.ts
@@ -0,0 +1,20 @@
+import express, { Router } from 'express';
+import { FileService } from './file';
+
+export class RouterService {
+    private router: Router;
+    private fileService: FileService;
+
+    constructor() {
+        this.router = express.Router();
+        this.fileService = new FileService();
+    }
+
+    setupRouter() {
+        this.fileService.setupRouter(this.router);
+    }
+
+    getRouter() {
+        return this.router;
+    }
+}
