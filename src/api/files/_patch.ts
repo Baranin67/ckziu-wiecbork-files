303db68dfdb9c61be4e49a68f36e271c3447ed88
Baranin67 <mati.baranowicz@gmail.com>
2025-05-01 20:38:01 +0200

    update: file update endpoint fix

--- src/api/files/_patch.ts
+++ src/api/files/_patch.ts
@@ -1,9 +1,5 @@
-import { FileService } from '../../services/file';
 import { NodeRequest, NodeResponse } from '../../types/api';
 
 export default async function (req: NodeRequest, res: NodeResponse) {
-    const fileService = new FileService();
-
-    fileService.updateFiles(req.body.filters, req.body.data);
     res.status(200);
 }
