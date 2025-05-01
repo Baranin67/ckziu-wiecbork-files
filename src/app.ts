a5d8960666c27b66bebfb607069c1b6790941e76
Baranin67 <mati.baranowicz@gmail.com>
2025-05-01 22:21:43 +0200

    update: app entry file fixes and upgrades
    Fixes:
    - environment variables are read properly
    - middleware is loaded properly
    
    Upgrades:
    - checking for undefined environment variables (if there are - print a relevant error message)

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,51 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import { exit } from 'process';
+import dotenv from 'dotenv';
 
 import { RouterController } from './controllers/router';
 
 // INICJALIZACJA
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+dotenv.config();
 
-// ROUTER
+const app = express();
+
+const serverPortRaw = process.env.SERVER_PORT;
+const corsAllowMethods = process.env.ALLOW_METHODS;
+const corsAllowOrigin = process.env.ALLOW_ORIGIN;
+
+// WERYFIKACJA ZMIENNYCH ŚRODOWISKOWYCH
+
+if (
+    serverPortRaw === undefined ||
+    corsAllowMethods === undefined ||
+    corsAllowOrigin === undefined
+) {
+    console.error(
+        '[Error] Some of the required environment variables not set: SERVER_PORT, ALLOW_METHODS and ALLOW_ORIGIN'
+    );
+    exit(1);
+}
+
+const serverPort = parseInt(serverPortRaw);
+
+if (isNaN(serverPort)) {
+    console.error('[Error] SERVER_PORT must be a number!');
+    exit(1);
+}
+
+// MIDDLEWARE
+
+app.use(express.json());
+app.use(
+    cors({
+        methods: corsAllowMethods,
+        origin: corsAllowOrigin
+    })
+);
+
+// ROUTERY
 
 const routerController = new RouterController();
 routerController.setupRouter();
@@ -17,23 +53,13 @@ routerController.setupRouter();
 const router = routerController.getRouter();
 app.use('/api', router);
 
-// MIDDLEWARE
-
-app.use(
-    cors({
-        methods: process.env.ALLOW_METHODS,
-        origin: process.env.ALLOW_ORIGIN
-    })
-);
-app.use(bodyParser.json());
-
 // START
 
-app.listen(PORT, err => {
+app.listen(serverPort, err => {
     if (err) {
         console.error(err.message);
         return;
     }
 
-    console.log(`Listening on port ${PORT}...`);
+    console.log(`Listening on port ${serverPort}...`);
 });
