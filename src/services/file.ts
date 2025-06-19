2a0c32073ce45631230b121521e26f6397cca8bc
Baranin67 <mati.baranowicz@gmail.com>
2025-06-20 00:33:57 +0200

    update: delete `get` endpoint

--- src/services/file.ts
+++ src/services/file.ts
@@ -1,5 +1,4 @@
 import _put from '../api/files/_put.js';
-import _get from '../api/files/_get.js';
 import _patch from '../api/files/_patch.js';
 import _delete from '../api/files/_delete.js';
 
@@ -7,6 +6,6 @@ import { Service } from './service.js';
 
 export class FileService extends Service {
     constructor() {
-        super('/files', { _put, _get, _patch, _delete });
+        super('/files', { _put, _patch, _delete });
     }
 }
